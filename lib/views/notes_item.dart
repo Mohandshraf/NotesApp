6561ea34eca1544a7772929d7c0cf565dd16b387
Mohandshraf <information123890@gmail.com>
2025-06-01 11:40:59 +0300

    feat: implement EditNoteView and EditNoteViewBody for note editing functionality

--- lib/views/notes_item.dart
+++ lib/views/notes_item.dart
@@ -6,51 +6,54 @@ class Notesitem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80), // لون الخلفية
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8, bottom: 8),
-                    child: Text(
-                      "Flutter Tips",
-                      style: TextStyle(color: Colors.black, fontSize: 25),
+    return GestureDetector(
+      onTap: () {} ,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80), // لون الخلفية
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 8),
+                      child: Text(
+                        "Flutter Tips",
+                        style: TextStyle(color: Colors.black, fontSize: 25),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 8), // 👈 المسافة بين العنوان والوصف
-                  Padding(
-                    padding: const EdgeInsets.only(top: 2, bottom: 8),
-                    child: Text(
-                      "Learn how to build beautiful apps with Flutter.",
-                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    SizedBox(height: 8), // 👈 المسافة بين العنوان والوصف
+                    Padding(
+                      padding: const EdgeInsets.only(top: 2, bottom: 8),
+                      child: Text(
+                        "Learn how to build beautiful apps with Flutter.",
+                        style: TextStyle(color: Colors.grey, fontSize: 15),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+                trailing: Icon(FontAwesomeIcons.trash, color: Colors.black),
               ),
-              trailing: Icon(FontAwesomeIcons.trash, color: Colors.black),
-            ),
-
-            Padding(
-              padding: EdgeInsets.only(right: 16, top: 10),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8, bottom: 10),
-                child: Text(
-                  "Created on: 2025-30-05",
-                  style: TextStyle(color: Colors.black, fontSize: 12),
+      
+              Padding(
+                padding: EdgeInsets.only(right: 16, top: 10),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 10),
+                  child: Text(
+                    "Created on: 2025-30-05",
+                    style: TextStyle(color: Colors.black, fontSize: 12),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
