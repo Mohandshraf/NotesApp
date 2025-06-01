85132ba09defb9edb5ac90b0a1b1cfeecd6d9bf5
Mohandshraf <information123890@gmail.com>
2025-06-01 12:32:55 +0300

    feat: enhance EditNoteViewBody with title input and update note model for structured data

--- lib/views/edit_note_view_body.dart
+++ lib/views/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/custom_app_bar.dart';
+import 'package:notes_app/views/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -9,7 +10,11 @@ class EditNoteViewBody extends StatelessWidget {
     return Container(
       color: Color.fromARGB(255, 90, 88, 88), // لون الخلفية
       child: Column(
-        children: [CostumAppBar(title: "Edit Note", icon: Icons.check)],
+        children: [
+          CostumAppBar(title: "Edit Note", icon: Icons.check),
+          CustomTextField(hintText: "Title"),
+          CustomTextField(hintText: "Content", maxLines: 6),
+        ],
       ),
     );
   }
