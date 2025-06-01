52e1220bed55f6f8c498271f98149554654098c3
Mohandshraf <information123890@gmail.com>
2025-06-01 12:46:19 +0300

    feat: refactor NoteModel to extend HiveObject and add TypeAdapter for serialization

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,19 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+
+
+part 'note_model.g.dart';
+
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel({
@@ -10,6 +22,4 @@ class NoteModel {
     required this.date,
     required this.color,
   });
-
-
 }
